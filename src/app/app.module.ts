198342aef757cbf0b2061ef0bc0ddcab65b23565
Basti Hoffmann <bo.hoffi@gmail.com>
2019-08-17 13:54:52 +0200

    added repo components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './components/root/app.component';
 import { LinksComponent } from './components/profile/links/links.component';
 import { BasicComponent } from './components/profile/basic/basic.component';
+import { RepoListComponent } from './components/repos/repo-list/repo-list.component';
+import { RepoItemComponent } from './components/repos/repo-item/repo-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LinksComponent,
-    BasicComponent
+    BasicComponent,
+    RepoListComponent,
+    RepoItemComponent
   ],
   imports: [
     BrowserModule
